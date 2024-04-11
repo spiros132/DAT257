f2fdd27964002e51fb749ce1486227de16b3f9d3
jlindqvi1 <jlindqvi@student.chalmers.se>
2024-04-11 20:46:30 +0200

    modified:   nutrition-diary/src/app/page.tsx 	modified:   nutrition-diary/src/components/SearchBar.tsx

--- nutrition-diary/src/app/page.tsx
+++ nutrition-diary/src/app/page.tsx
@@ -40,10 +40,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
               <SearchBar onSearch={onSearch} /> 
               <div className="items-center min-h-screen">
-              <label className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <label className="block p-2.5 text-sm text-gray-900 bg-gray-50  border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
               Food name: {foodName}
               </label>
-              <label className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <label className="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
               Calories: {calories}
               </label>
             </div>
