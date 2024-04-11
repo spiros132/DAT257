3437f07a499d61e3f278282953bc0a064468750d
jlindqvi1 <jlindqvi@student.chalmers.se>
2024-04-11 21:31:26 +0200

    modified:   nutrition-diary/src/app/page.tsx 	new file:   nutrition-diary/src/components/ResultDisplay.tsx

--- nutrition-diary/src/app/page.tsx
+++ nutrition-diary/src/app/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 import SearchBar from "@/components/SearchBar";
+import ResultDisplay from "@/components/ResultDisplay";
 import React, {useState} from "react";
 
 export default function Home() {
-  const onSearch = (searchInput: string) => { fetchData(searchInput); };
-  const [foodName, setFoodName] = useState<string>('');
-  const [calories, setCalories] = useState<string>('');
 
-  function handleResult(result: any){
-    result.foods.forEach((food: any) => {
-      setFoodName(food.food_name);
-      setCalories(food.nf_calories);});
-  }
+  const onSearch = (searchInput: string) => { fetchData(searchInput); };
+  const [result, setResult] = useState<string>('');
 
   function fetchData(searchInput: string){
-    console.log(searchInput);
     const myHeaders = new Headers();
     myHeaders.append("x-app-id", "12e033fb");
     myHeaders.append("x-app-key", "039977dee92c04ad730011a5f77c3855	");
@@ -32,21 +26,14 @@ export default function Home() {
   
     fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", requestOptions)
     .then((response) => response.json())
-    .then((result) => handleResult(result))
+    .then((result) => setResult(result))
     .catch((error) => console.error(error));
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
               <SearchBar onSearch={onSearch} /> 
-              <div className="items-center min-h-screen">
-              <label className="block p-2.5 text-sm text-gray-900 bg-gray-50  border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-              Food name: {foodName}
-              </label>
-              <label className="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-              Calories: {calories}
-              </label>
-            </div>
+              <ResultDisplay result = {result}/>
     </main>
   );
 }
---
+++ nutrition-diary/src/components/ResultDisplay.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, {useState, useEffect} from "react";
+
+export default function ResultDisplay(props: {result: any}){
+
+    const [foodName, setFoodName] = useState<string>('');
+    const [calories, setCalories] = useState<string>('');
+    const [photo, setPhoto] = useState<string>('');
+
+    useEffect(() => {
+        handleResult(props.result);
+    }, [props.result]); 
+
+    function handleResult(result: any){
+        result.foods.forEach((food: any) => {
+            setPhoto(food.photo.thumb);
+            setFoodName(food.food_name);
+            setCalories(food.nf_calories);
+        });
+    }
+
+    return (
+        <div className="items-center min-h-screen">
+        <img src={photo} alt="no image" className="block w-32 h-32"/>
+        <label className="block p-2.5 text-sm text-gray-900 bg-gray-50  border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+        Food name: {foodName}
+        </label>
+        <label className="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+        Calories: {calories}
+        </label>
+        </div>
+    );
+
+}
+    
+
