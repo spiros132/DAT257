86b29c7a78817ba2a84893b83fb07dedd920a5a9
Spyridon Siarapis <spiros.siarapis@gmail.com>
2024-04-13 11:29:55 +0200

    if nothing is chosen as a food the display should show nothing

--- nutrition-diary/src/components/ResultDisplay.tsx
+++ nutrition-diary/src/components/ResultDisplay.tsx
@@ -5,7 +5,7 @@ import { SearchFoodItemNutrientsData, SearchFoodItemNutrients } from '@/app/inte
 
 export default function ResultDisplay(props: {readonly result: string}){
     const [foodName, setFoodName] = useState<string>('');
-    const [calories, setCalories] = useState<number>(0);
+    const [calories, setCalories] = useState<number>(-1);
     const [photo, setPhoto] = useState<string>('');
 
     useEffect(() => {
@@ -22,17 +22,24 @@ export default function ResultDisplay(props: {readonly result: string}){
         });
     }
 
-    return (
-        <div className="items-center min-h-screen">
-        <img src={photo} alt="Food" className="block w-32 h-32"/>
-        <label className="block p-2.5 text-sm text-gray-900 bg-gray-50  border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-        Food name: {foodName}
-        </label>
-        <label className="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-        Calories: {calories}
-        </label>
-        </div>
-    );
+
+    if(photo != "" && foodName != "" && calories != -1)
+        return (
+            <div className="items-center min-h-screen">
+                <img src={photo} alt="Food" className="block w-32"/>
+                <label className="block p-2.5 text-sm text-gray-900 bg-gray-50  border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+                    Food name: {foodName}
+                </label>
+                <label className="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+                    Calories: {calories}
+                </label>
+            </div>
+        );
+    else
+        return (
+            <>
+            </>
+        );
 
 }
     
