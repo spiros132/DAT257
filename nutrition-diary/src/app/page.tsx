19cf3347ba1b98163080c51d6b82febb0bd1f64c
Spyridon Siarapis <spiros.siarapis@gmail.com>
2024-04-26 21:28:41 +0200

    added the "home" page

--- nutrition-diary/src/app/page.tsx
+++ nutrition-diary/src/app/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 export default function Home() {
+  // The main page when you go into the website
   return (
-    <>
-    </>
+    <div>
+      <div className="flex-1 flex items-center justify-center"> {/* Center the image */}
+        <img src="/NutritionDiary1.png" alt="Nutrition Diary Logo" className="centered-image" style={{ width: '450px', filter: 'grayscale(3%) brightness(95%)' }} />
+      </div>
+    </div>
   );
 }
