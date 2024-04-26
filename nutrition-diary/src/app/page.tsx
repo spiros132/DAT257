c360c9ed8ca6726be13f09d36305383b2369737a
Spyridon Siarapis <spiros.siarapis@gmail.com>
2024-04-26 21:28:09 +0200

    Removed unused imports

--- nutrition-diary/src/app/page.tsx
+++ nutrition-diary/src/app/page.tsx
@@ -1,37 +1,6 @@
 "use client";
-import SearchBar from "@/components/SearchBar";
-import ResultDisplay from "@/components/ResultDisplay";
-import MealCard from "@/components/MealCard";
-import CalorieCounter from "@/components/CalorieCounter";
-
-import React, {startTransition, useState} from "react";
-import { SearchForFood } from "./server/actions";
-import LoginPage from "@/app/login/page";
-import CreateAccountPage from "@/app/register/page";
-import MenuPage from "@/app/menu/page";
-import MainPage from "@/app/dashboard/page";
-import AddMealButton from "@/components/AddMealButton";
-import CreateEditMealPage from "@/app/mealpage/page";
-
 
 export default function Home() {
-
-  const onSearch = (searchInput: string) => { fetchData(searchInput); };
-  const [result, setResult] = useState<string>('');
-
-  function fetchData(searchInput: string){
-    startTransition(() => {
-      SearchForFood(searchInput)
-      .then((res: string | undefined) => {
-        if(res != undefined)
-          setResult(res);
-      })
-      .catch((error: Error) => {
-        console.log(error);
-      });
-    });
-  }
-
   return (
     <>
     </>
