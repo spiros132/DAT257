3c6806fde534e6899bfd5f8e2fc46b2803ad32c8
Nenwa <nenwa@chalmers.se>
2024-05-06 14:51:07 +0200

    Added a description to every function in db

--- nutrition-diary/src/app/lib/database.ts
+++ nutrition-diary/src/app/lib/database.ts
@@ -6,13 +6,17 @@ import { open, Database } from "sqlite";
 
 export type databaseReturnType = any[] | undefined;
 
+// Function to create db tables if they don't exist
 function createDB(){
+    // initialize new SQLite database instance using the provided location
     const newDB = new sqlite3.Database(database_location, OPEN_CREATE | OPEN_READWRITE, (err) => {     
+        // if error: log the error message
         if (err) {        
             console.error(err.message);     
         }     
     });
 
+    // Serialize db operations to make sure they execute in sequence
     newDB.serialize(() => {
         newDB.run(
             `CREATE TABLE IF NOT EXISTS users(
@@ -108,20 +112,24 @@ function createDB(){
             FOREIGN KEY (userId) REFERENCES users(id)
         `);
 };
-
+// Initialize the database and create the tables
 createDB();
 let db : Database | null = null;
 
+// Function to open the db connection
 async function openDB(){
     if (!db) {
         // If the database instance is not initialized, open the database connection
         db = await open({
-          filename: database_location, // Specify the database file path
-          driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
+        // Specify the database file path
+          filename: database_location, 
+          // Specify the database driver 
+          driver: sqlite3.Database, 
         });
     }
 }
 
+// Function to close the db connection
 function closeDB(){
     if(db){
         db.close();
@@ -129,6 +137,7 @@ function closeDB(){
     }
 }
 
+// Function to execute to SQL query
 async function executeQuery(query : string, params : any, timeout : number = 5000): Promise<any[] | undefined> {
     try{
         await openDB();
@@ -149,31 +158,37 @@ async function executeQuery(query : string, params : any, timeout : number = 500
     }
 }
 
+// Function to register the user's eaten meal
 export async function registerEatenMeal(user: number, name: string, date: Date){
   await executeQuery(`INSERT INTO eatenMeals(user, name, date) VALUES(?, ?, ?, ?)`, [user, name, date])
 }
 
+// Function to register an item of a meal eaten by the user
 export async function registerEatenMealItem(meal: number, food: string, quantity: number = 0){
   await executeQuery(`INSERT INTO eatenMealItem(meal, food, quantity) VALUES(?, ?, ?, ?)`, [meal, food, quantity])
 }
 
+// Function to view the meals eaten by the user
 export async function viewMeals(user: string){
   return executeQuery(`SELECT eatenMeals.name FROM eatenMeals
                        INNER JOIN users ON eatenMeals.user == users.id
                        WHERE username == ?`, [user]);
 }
 
+// Function to view the meal items (food items) of the meal eaten by the user
 export async function viewFoodsOfMeal(meal: string){
   return executeQuery(`SELECT eatenMealItem.name FROM eatenMealItem
                        INNER JOIN eatenMeals ON eatenMealItem.meal == eatenMeals.id
                        WHERE meal == ?`, [meal]);
 }
 
+// Function to delete a meal
 export async function deleteMeal(mealname: string){
   await executeQuery(`DELETE FROM eatenMeals WHERE name == ?`, 
   [mealname])
 }
 
+// Function to add a token for the user's authentication
 async function deleteOldTokens() {
     await errorHandler(async () => {
         return await executeQuery(`DELETE FROM tokens WHERE valid <= datetime('now')`, []);
@@ -189,6 +204,7 @@ export async function addToken(token: string, userID: number) {
     });
 }
 
+// Function to get token data
 export async function getTokenData(token: string) {
     return await errorHandler(async () => {
         await deleteOldTokens();
@@ -196,6 +212,7 @@ export async function getTokenData(token: string) {
     });
 }
 
+// Function to get token from user id
 export async function getTokenFromUserID(userID: number) {
     return await errorHandler(async () => {
         await deleteOldTokens();
@@ -203,10 +220,12 @@ export async function getTokenFromUserID(userID: number) {
     });
 }
 
+// Function to salt hashed password
 function generateSalt(): string {
     return Math.random().toString(36).substring(2, 10); 
 }
 
+// Function to register a user
 export async function registerUser(username: string, password: string, height: number = 0, weight: number = 0){
     return await errorHandler(async () => {
         // const salt = generateSalt();
@@ -215,14 +234,17 @@ export async function registerUser(username: string, password: string, height: n
     });
 }
 
+// Function to update the user's height
 export async function updateHeight(username: string, height: number){
     executeQuery(`UPDATE users SET height = ? WHERE username = ?`, [height, username]);
 }
 
+// Function to update the user's weight
 export async function updateWeight(username: string, weight: number){
     executeQuery(`UPDATE users SET weight = ? WHERE username = ?`, [weight, username]);
 }
 
+// Function to update the user's password
 export async function updatePassword(username: string, password: string) {
     await errorHandler(async () => {
         password = createHash('sha256').update(password).digest('hex');
@@ -230,6 +252,7 @@ export async function updatePassword(username: string, password: string) {
     });
 }
 
+// Login function
 export async function loginUser(username: string, password: string) {
     const result = await errorHandler(async () => {
         password = createHash('sha256').update(password).digest('hex');
@@ -239,15 +262,14 @@ export async function loginUser(username: string, password: string) {
     return result;
 }
 
+// Function to get the user's information
 export async function getUserInfo(userID: number = -1, username: string = ""){
     return errorHandler(async () => {
         return await executeQuery(`SELECT username, height, weight FROM users WHERE id = ? or username = ?`, [userID, username]);
     });
 }
 
-
-
-
+{/** Function to: */}
 // Save a meal starting with inserting the meal into the savedMeael table
     async function saveMeal(userId: number, name: string, description: string, calories: number, protein: number, carbohydrates: number, fat:number ,items: { food: string, quantity: number }[]) {
         const mealInsertResult = await executeQuery(
@@ -255,7 +277,7 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
             [userId, name, description, calories, protein, fat, carbohydrates]
         );
 
-        // Check result isn't empty & then get lastId
+        // Check if the result isn't empty & then get lastId
         if (mealInsertResult && mealInsertResult.length > 0) {
             const mealId = mealInsertResult[0].lastID; 
 
@@ -270,7 +292,7 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
         throw new Error("Failed to insert the meal into the database.");
     }
 
-    // Fetch user's saved meals 
+// Fetch user's saved meals 
     async function getSavedMeals(userId: number) {
         return executeQuery(
             `SELECT id, name, description FROM savedMeals WHERE user = ?`,
@@ -278,7 +300,7 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
         );
     }
 
-    // Fetch mealItems for a saved meal
+// Fetch mealItems for a saved meal
     async function getMealItems(mealId: number) {
         return executeQuery(
             `SELECT food, quantity FROM savedMealItem WHERE meal = ?`,
@@ -342,12 +364,15 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
 // Retrieve user's progress interval
     export async function getUserProgress(userId:number, datetime: string){
         let startProgInterval, endProgInterval;
+        // Determine start-&end-time of a specific interval
         switch (datetime){
             case 'Today':
+            // start- and end-Date are set to the current date 
                 startProgInterval  = new Date();
                 endProgInterval    = new Date();
                 break;
             case 'Weekly':
+            // start-Date is set 7 days past the current date, while end-Date is set today
                 startProgInterval  = new Date();
                 startProgInterval.setDate(startProgInterval.getDate()-7);
                 endProgInterval    = new Date();
@@ -394,9 +419,11 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
         );
     }
 
-// Store user's progress
+// Store user's progress in db
     export async function storeUserProgress(userId:number, progressValue: number) {
+    // get current timestamp in ISO string fromate (YYYY-MM-DDTHH:MM:SS.sssZ)
         const CURRENT_TIMESTAMP = new Date().toISOString();
+        // execute the db query to insert user's progress
         await executeQuery(
             `   INSERT INTO userProgress (userId, progressValue, date) VALUES (?,?,?)`,
             [userId, progressValue, CURRENT_TIMESTAMP]
@@ -406,29 +433,36 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
 // Retrieve user's progress intervall
     export async function getProgInterval(userId: number, interval: string) {
         let startDateSlope, endDateSlope;
+        // Select the date range for the specified interval
         switch (interval) {
             case 'today':
+            // For today's interval, both start- and end- dates are set to today
                 startDateSlope = new Date().toISOString().split('T')[0];
                 endDateSlope = startDateSlope;
                 break;
             case 'this_week':
+            // For the week's intervall, the start- and end-dates are calculated based on the current day
                 startDateSlope = new Date();
-                startDateSlope.setDate(startDateSlope.getDate() - startDateSlope.getDay()); // Start on sunday
+                startDateSlope.setDate(startDateSlope.getDate() - startDateSlope.getDay()); // ex. start on sunday
                 endDateSlope = new Date();
-                endDateSlope.setDate(endDateSlope.getDate() + (6 - endDateSlope.getDay())); // End on saturday
+                endDateSlope.setDate(endDateSlope.getDate() + (6 - endDateSlope.getDay())); // end on saturday
+                // Convert dates to ISO string format (YYYY-MM-DD)
                 startDateSlope = startDateSlope.toISOString().split('T')[0];
                 endDateSlope = endDateSlope.toISOString().split('T')[0];
                 break;
             case 'this_month':
+            // For the month's interval, the star- & end-date are set based on the last day of the current month
                 startDateSlope = new Date();
                 startDateSlope.setDate(1); // Start of the month
                 endDateSlope = new Date();
-                endDateSlope.setMonth(endDateSlope.getMonth() + 1); // End of the month
+                endDateSlope.setMonth(endDateSlope.getMonth() + 1); // End of the month, move to next month
                 endDateSlope.setDate(0); // Last day of the previous month
+                // convert the date to an ISO 8601 string formate (YYY-MM-DD)
                 startDateSlope = startDateSlope.toISOString().split('T')[0];
                 endDateSlope = endDateSlope.toISOString().split('T')[0];
                 break;
             default:
+            // In case of invalid interval types: 
                 throw new Error('Invalid interval type');
         }
     
@@ -439,13 +473,17 @@ export async function getUserInfo(userID: number = -1, username: string = ""){
         );
     }
 
+// Function to handle errors in async db operations
 type errorHandlerFunction = () => Promise<databaseReturnType | undefined>;
+// the function is executed for errors
 async function errorHandler(fn: errorHandlerFunction): Promise<databaseReturnType | undefined> {
     try {
+    // execute function and return it's resutls
         return await fn();
     }
     catch(e) {
-        console.log(e); // or maybe console.e("Error:", e); ?
+    // log error to the console and then return undefined in case of error..
+        console.log(e); // Alternatively, use console.e("Error:", e); ?
         return undefined;
     }
 }
