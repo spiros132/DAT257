89032600d77a839f376c030faaff4983c4c5d8db
Spyridon Siarapis <spiros.siarapis@gmail.com>
2024-05-13 12:57:07 +0200

    fixed the logout function

--- nutrition-diary/src/app/logout/page.tsx
+++ nutrition-diary/src/app/logout/page.tsx
@@ -3,11 +3,9 @@ import { startTransition } from "react";
 import { LogoutUser } from "@/app/actions/users";
 
 export default function Page(){
-
-    window.onload = function() {
+    window.onload = () => {
         startTransition(() => {
-            LogoutUser()
-            .then();
+            LogoutUser();
         });
     }
 
